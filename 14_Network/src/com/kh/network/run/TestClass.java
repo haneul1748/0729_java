547b023a7bb232ece5348159e91813fd776b9c76
이름 <>
2025-08-21 11:16:41 +0900

    Homework

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -4,7 +4,14 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Home Sweet Home ~~~~ 
+		
+		// 왜 안바뀜?
+		// 아니 머임?
 	}
-
+	public void hi() {
+		
+	}
+	
+	
 }
