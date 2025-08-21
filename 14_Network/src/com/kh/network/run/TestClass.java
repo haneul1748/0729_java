2caf0ed574f9214f40737fd1ce619321feb5bd29
이름 <>
2025-08-21 11:43:36 +0900

    git사용법

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -7,5 +7,12 @@ public class TestClass {
 		// 아 오늘도 열심히 공부했다 아 힘들다 고생했다.
 		// 안바
 	}
+	public void printStatement () {
+		System.out.println("1. 워크스페이스에서 gitBash를 실행한다.");
+		System.out.println("2. git pull origin master");
+		System.out.println("3. git add .");
+		System.out.println("4. git commit -m 뭐했는지");
+		System.out.println("5. git push origin master");
+	}
 
 }
